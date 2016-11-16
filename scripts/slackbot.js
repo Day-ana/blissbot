2c13de1d617e0aa6e8de1a0fd5161c2a3429a6a7
Dayana Lorza <daydesignz@gmail.com>
2016-11-15 20:53:32 -0800

    committing bot 4

--- scripts/slackbot.js
+++ scripts/slackbot.js
@@ -18,13 +18,13 @@
 // <github username of the original script author>
 //
 
-module.exports = function(robot) {
-  //  YOUR CODE HERE
-  //  Example
-   robot.hear(/blissme/i, function(msg) {
-     return msg.send("You are not special. You're not a beautiful and unique snowflake. You're the same decaying organic matter as everything else.");
-   });
-}
+// module.exports = function(robot) {
+//   //  YOUR CODE HERE
+//   //  Example
+//    robot.hear(/blissme/i, function(msg) {
+//      return msg.send("You are not special. You're not a beautiful and unique snowflake. You're the same decaying organic matter as everything else.");
+//    });
+// }
 
 // bot.hear(/Hello!/, function(res) {
 //    return res.send("Hi there!");
@@ -42,9 +42,9 @@ nature = ["http://lorempixel.com/400/400/nature/"];
 
 module.exports = function(robot) {
   // Basic example of respond / send. If the user enters hi or hello the bot responds "Howdy!" 
-  // return robot.respond(/hi|hello/i, function(msg) {
-  //   return msg.send("Howdy!");
-  // });
+  return robot.respond(/hola/i, function(msg) {
+    return msg.send("Howdy!");
+  });
 
   // Random Example
   //If a user enters 'ship it' we return a random squirrel, which is popular for symbolizing shipping something with engineers
