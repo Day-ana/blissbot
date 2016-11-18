7315b90a56546d6abfcbdb1047f6a8b8494651f6
Dayana Lorza <daydesignz@gmail.com>
2016-11-17 19:12:18 -0800

    new commit

--- scripts/slackbot.js
+++ scripts/slackbot.js
@@ -42,13 +42,13 @@ nature = ["http://lorempixel.com/400/400/nature/", "http://lorempixel.com/400/40
 
 module.exports = function(robot) {
   // Basic example of respond / send. If the user enters hi or hello the bot responds "Howdy!" 
-  // return robot.respond(/hola/i, function(msg) {
-  //   return msg.send("Howdy!");
-  // });
+  return robot.respond(/blissme/i, function(msg) {
+    return msg.send("Howdy!");
+  });
 
   // Random Example
   //If a user enters 'ship it' we return a random squirrel, which is popular for symbolizing shipping something with engineers
-  return robot.hear(/bliss/i, function(msg) {
+  return robot.hear(/blissful/i, function(msg) {
     return msg.send(msg.random(nature));
   });
 };
